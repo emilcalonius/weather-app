e3e73836d5f48b5b492a1d4ea204f8f69074b450
Emil Calonius <emil.calonius@tuni.fi>
2022-02-16 00:59:51 +0200

    Added selecting city on large screen view

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,6 @@
 /*
 
-Top navigation on large screens and menu openDrawering from the side on mobile.
+Top navigation on large screens and menu drawerOpening from the side on mobile.
 
 */
 
@@ -10,21 +10,31 @@ import Drawer from '@mui/material/Drawer';
 import MenuIcon from '@mui/icons-material/Menu';
 import IconButton from '@mui/material/IconButton';
 import list from "./list.txt";
-import { Button, Dialog, List } from "@mui/material";
+import { Button, Dialog, List, Menu, MenuItem } from "@mui/material";
 import { Box } from "@mui/system";
 
 function Navigation(props) {
-  const [ openDrawer, setOpenDrawer ] = useState(false);
+  const [ drawerOpen, setdrawerOpen ] = useState(false);
   const [ dialogOpen, setDialogOpen ] = useState(false);
+  const [ anchorEl, setAnchorEl ] = useState(null);
   const [ cities, setCities ] = useState([]);
   const isMobile = useMediaQuery({query: "(max-width:600px)"});
+  const menuOpen = Boolean(anchorEl);
 
   const toggleDrawer = () => {
-    setOpenDrawer(!openDrawer);
+    setdrawerOpen(!drawerOpen);
   };
 
   const toggleDialog = () => {
     setDialogOpen(!dialogOpen);
+  };
+
+  const openMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
   };
 
   useEffect(() => {
@@ -38,20 +48,42 @@ function Navigation(props) {
       {
         !isMobile ?
           <div>
-            <button className="btn">Valitse kaupunki</button>
+            <button className="btn" onClick={openMenu}>Valitse kaupunki</button>
             <button className="btn">Avaa kartta</button>
+            <Menu
+              anchorEl={anchorEl}
+              open={menuOpen}
+              onClose={handleMenuClose}
+            >
+              {
+                cities.map(city => {
+                  return(
+                    <Box key={city}>
+                      <MenuItem
+                        onClick={() => {
+                          props.updateCity(city);
+                          handleMenuClose();
+                        }}
+                      >
+                        {city}
+                      </MenuItem>
+                    </Box>
+                  );
+                })
+              }
+            </Menu>
           </div>
           :
           <div>
             <IconButton
               onClick={toggleDrawer}
-              sx={openDrawer ? { display: 'none' } : {position: "absolute", right: 0}}
+              sx={drawerOpen ? { display: 'none' } : {position: "absolute", right: 0}}
             >
               <MenuIcon sx={{color: "blanchedalmond"}} />
             </IconButton>
             <Drawer
               anchor={"right"}
-              open={openDrawer}
+              open={drawerOpen}
               onClose={toggleDrawer}
             >
               <button className="btn" onClick={toggleDialog}>Valitse kaupunki</button>
